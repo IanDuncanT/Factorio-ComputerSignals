b408ebab01541989ee663d4d82facc1e4d1a1b15
combinatorMasterRace <CombinatorMasterRace@gmail.com>
2018-04-30 07:53:13 -0500

    Created category and files with data:extend({})

---
+++ category.lua
@@ -0,0 +1,105 @@
+data:extend({
+  {
+    type = "item-group",
+    name = "combinator-signals",
+    order = "ld",
+    icon = "",
+    icon_size = 128
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "red-numbers",
+    order = "a"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "red-letters",
+    order = "b"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "red-binary",
+    order = "c"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "red-roboport",
+    order = "d"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "yellow-numbers",
+    order = "e"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "yellow-letters",
+    order = "f"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "yellow-binary",
+    order = "g"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "yellow-roboport",
+    order = "h"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "blue-numbers",
+    order = "i"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "blue-letters",
+    order = "j"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "blue-binary",
+    order = "k"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "blue-roboport",
+    order = "l"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "green-numbers",
+    order = "m"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "green-letters",
+    order = "n"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "green-binary",
+    order = "o"
+  },
+  {
+    type = "item-subgroup",
+    group = "combinator-signals",
+    name = "green-roboport",
+    order = "p"
+  },
+})
---
+++ data.lua
@@ -0,0 +1,4 @@
+require("prototypes.red")
+require("prototypes.blue")
+require("prototypes.green")
+require("prototypes.yellow")
